1cf08905d667fe0983772e7d2c769423915ca686
shrutikwagh <91433395+shrutikwagh@users.noreply.github.com>
2022-03-30 19:21:21 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'eshop2',
         'USER':'root',
-        'PASSWORD':'123456',
+        'PASSWORD':' ',
         'HOST':'localhost',
         'PORT':'3306',
     }
