b5ab8c9ca3df5ff3f837c4a3c36e2037b10fdf3a
shrutikwagh <91433395+shrutikwagh@users.noreply.github.com>
2023-11-30 23:57:17 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shrutikawagh.5920@gmail.com'
-EMAIL_HOST_PASSWORD = 'Shruti@6460'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
